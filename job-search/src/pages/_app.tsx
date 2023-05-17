8f13ffa68bc43b67384246d2df33618cf1b8d6af
Bulrock <maksim.volozhinskij@gmail.com>
2023-05-17 16:07:22 +0200

    feat: install mantine library dependencies

--- job-search/src/pages/_app.tsx
+++ job-search/src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
 
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'light',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+
   );
 }
