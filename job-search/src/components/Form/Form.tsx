72dbfba6f649dc3a52378bdd67780f6a99f1d3bb
Bulrock <maksim.volozhinskij@gmail.com>
2023-05-19 18:32:46 +0200

    feat: implement filter form reset on reset button click

--- job-search/src/components/Form/Form.tsx
+++ job-search/src/components/Form/Form.tsx
@@ -32,6 +32,10 @@ export default function Form({ onFilterSubmit }: IFormProps) {
     },
   });
 
+  function onFormReset() {
+    form.reset();
+  }
+
   useEffect(() => {
     setIsLoadingCatalog(true);
     cataloguesService()
@@ -53,7 +57,7 @@ export default function Form({ onFilterSubmit }: IFormProps) {
       <form onSubmit={form.onSubmit((values) => onFilterSubmit(values))}>
         <div className={styles.formHeader}>
           <span className={styles.formTitle}>Фильтры</span>
-          <div className={styles.formResetWrapper}>
+          <div onClick={onFormReset} className={styles.formResetWrapper}>
             <span className={styles.formReset}>Сбросить все</span>
             <Image width={16} height={16} src={iconCloseForm} alt="icon close" />
           </div>
