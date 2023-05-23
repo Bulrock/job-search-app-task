44faf056286052bf87af5899fd1c1a5257025731
Bulrock <maksim.volozhinskij@gmail.com>
2023-05-24 01:22:32 +0200

    fix: API pagination bug

--- src/components/Pagination/VacanciesNavigation.tsx
+++ src/components/Pagination/VacanciesNavigation.tsx
@@ -16,7 +16,7 @@ export default function VacanciesNavigation({
 }: IVacanciesNavigationProps) {
   return (
     <div className={classes.navigationWrapper}>
-      <Pagination value={page} onChange={onPageChange} total={total} />
+      <Pagination value={page} onChange={onPageChange} total={total <= 125 ? total : 125} />
     </div>
   );
 }
